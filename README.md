5c4a004df5e0a28206151c87919c6691c8be9d63
Alexander <alexander.khmelevskiy@gmail.com>
2024-08-31 18:45:03 +0400

    Init

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # airflow_custom_metrics_plugin
-This plugin add cpu and ram stats  for every tasks in airflow
+
+This plugin add cpu and ram stats for every tasks in airflow
+
+---
+
+# Custom Metrics Plugin for Apache Airflow
+
+## Overview
+
+The `CustomMetricsPlugin` is an Apache Airflow plugin designed to monitor the CPU and memory usage of tasks in
+real-time. This plugin gathers these metrics and pushes them to StatsD, where they can be visualized using monitoring
+tools such as Grafana. Also, it prints the metrics to the logs (I'm not sure if it's useful, but it's there :D).
+
+## Features
+
+- **CPU Usage Monitoring**: Tracks and reports the CPU usage of tasks.
+- **Memory Usage Monitoring**: Tracks and reports the memory usage of tasks.
+- **Automatic Cleanup**: Resets metrics to `0` when tasks are completed, skipped, failed, or deferred.
+- **Customizable**: Metrics names are configurable, and the plugin can be enabled/disabled via environment variables.
+
+## Installation
+
+1. **Clone the Repository**:
+   ```bash
+   git clone https://github.com/yourusername/custom-metrics-plugin.git
+   cd custom-metrics-plugin
+   ```
+
+2. **Add the Plugin to Your Airflow Plugins Directory**:
+   Copy the `custom_metrics_plugin.py` file into your Airflow plugins directory (usually located at
+   `/path/to/airflow/plugins/`).
+
+3. **Configure StatsD**:
+   Make sure your Airflow instance is configured to use StatsD. This typically involves setting the following in your
+   `airflow.cfg`:
+
+   ```ini
+   [metrics]
+   statsd_on = True
+   statsd_host = localhost
+   statsd_port = 8125
+   statsd_prefix = airflow
+   ```
+
+4. **Enable the Plugin**:
+   The plugin can be enabled or disabled using an environment variable. To enable it, set the
+   `CUSTOM_METRICS_PLUGIN_ENABLED` environment variable to `true`
+
+5. **Setting 'mappingConfig'** for statsd
+   ```
+    - match: "*.custom_metrics_cpu_usage_percent.*.*"
+      match_metric_type: gauge
+      name: "tasks_cpu_usage_percent"
+      labels:
+        airflow_id: "$1"
+        dag_id: "$2"
+        task_id: "$3"
+    - match: "*.custom_metrics_memory_usage_bytes.*.*"
+      match_metric_type: gauge
+      name: "tasks_memory_usage_percent"
+      labels:
+        airflow_id: "$1"
+        dag_id: "$2"
+        task_id: "$3"
+   ```
+
+## Usage
+
+Once the plugin is installed and enabled, it automatically starts monitoring tasks in your Airflow DAGs. The CPU and
+memory usage metrics will be sent to StatsD under the following names:
+
+- `tasks_cpu_usage_percent`: The CPU usage percentage of a task.
+- `tasks_memory_usage_percent`: The memory usage of a task in bytes.
+
+The metrics are tagged with the following labels:
+
+- `airflow_id`: The ID of the Airflow instance.
+- `dag_id`: The ID of the DAG.
+- `task_id`: The ID of the task.
+
+### Example Metric Names
+
+Given a DAG with `dag_id="example_dag"` and a task with `task_id="example_task.test"`, the metrics would be named:
+
+- `tasks_cpu_usage_percent.example_dag.example_task__test`
+- `tasks_memory_usage_percent.example_dag.example_task__test`
+
+## Configuration
+
+### Environment Variables
+
+- **`CUSTOM_METRICS_PLUGIN_ENABLED`**: Enables or disables the plugin. Set to `true` to enable.
+- **`CUSTOM_METRICS_PLUGIN_SLEEP_INTERVAL_SECONDS`**: Controls how often the plugin polls for CPU and memory usage data.
+  Default is `1` second.
+
+### Metric Name Customization
+
+The plugin replaces `.` in `dag_id` and `task_id` with `__` to ensure compatibility with monitoring tools that do not
+support `.` in metric names.
+
+---
+
+## Example usage in Grafana
+
+### Variables
+
+```
+label_values(airflow_id) - airflow_id
+label_values(tasks_cpu_usage_percent{airflow_id="$airflow_id"}, dag_id) - dag_id
+label_values(tasks_cpu_usage_percent{airflow_id="$airflow_id", dag_id="$dag_id"}, task_id) - task_id
+
+tasks_cpu_usage_percent{airflow_id="$airflow_id", dag_id="$dag_id", task_id="$task_id"} - For dashboard `CPU`
+tasks_memory_usage_percent{airflow_id="$airflow_id", dag_id="$dag_id", task_id="$task_id"} - For dashboard `RAM`
+topk(10, max_over_time(max by(dag_id, task_id) (tasks_cpu_usage_percent{airflow_id="$airflow_id"})[24h])) - For dashboard `Top 10 CPU [24h]`
+topk(10, max_over_time(max by(dag_id, task_id) (tasks_memory_usage_percent{airflow_id="$airflow_id"})[24h])) - For dashboard `Top 10 RAM [24h]`
+```
+
+### Dashboard
+
+![img.png](img.png)
---
+++ custom_metrics_plugin.py
@@ -0,0 +1,169 @@
+import logging
+
+from airflow.listeners import hookimpl
+from airflow.models import TaskInstance
+from airflow.plugins_manager import AirflowPlugin
+from airflow.stats import Stats
+from airflow.utils.state import TaskInstanceState
+
+
+STOPPED_TASK_INSTANCES_STATUSES = {
+    TaskInstanceState.REMOVED,
+    TaskInstanceState.RESTARTING,
+    TaskInstanceState.UP_FOR_RETRY,
+    TaskInstanceState.UP_FOR_RESCHEDULE,
+    TaskInstanceState.UPSTREAM_FAILED,
+    TaskInstanceState.SKIPPED,
+    TaskInstanceState.DEFERRED,
+}  # First added for the SKIPPED status, because airflow just terminated the process, and we should reset the metric.
+# The rest of the statuses are for insurance, nothing should break.
+
+
+def convert_bytes_to_readable_value(size_bytes: int) -> str:
+    """
+    Convert bytes to human readable value. Max supported value is '1023.99 PB'.
+
+    Parameters
+    ----------
+    size_bytes: int
+        Size in bytes.
+
+    Returns
+    -------
+    str
+        Human readable value
+    """
+
+    import math
+
+    if size_bytes == 0:
+        return "0B"
+    size_name = ("B", "KB", "MB", "GB", "TB", "PB")
+    i = int(math.floor(math.log(size_bytes, 1024)))
+    p = math.pow(1024, i)
+    s = round(size_bytes / p, 2)
+
+    return f"{s} {size_name[i]}"
+
+
+class TaskMonitorListener:
+    def __init__(self):
+        self.monitor_threads = {}
+
+    @property
+    def check_plugin_enabled(self):
+        """
+        If the plugin is disabled via an environment variable or if the environment variable is not set, we exit
+        """
+
+        import os
+
+        return os.getenv("CUSTOM_METRICS_PLUGIN_ENABLED", "False").lower() in ["true", "1", "t", "y", "yes"]
+
+    def get_metric_name(self, task_instance: TaskInstance, metric_name: str) -> str:
+        """
+        Get metric name for task_instance.
+        In statsd configuration they have name: `tasks_cpu_usage_percent` and `tasks_memory_usage_percent`.
+        Replace `.` to `__` in dag_id and task_id, because with `.` in name metrics are not displayed in Grafana.
+
+        Parameters
+        ----------
+        task_instance: TaskInstance
+            Task instance object.
+        metric_name: str
+            Metric name.
+
+        Returns
+        -------
+        str
+            Formatted metric name.
+        """
+        return f"{metric_name}.{str(task_instance.dag_id).replace('.',"__")}.{str(task_instance.task_id).replace('.',"__")}"
+
+    def _finalize_task(self, task_instance: TaskInstance) -> None:
+        """
+        Finalize task monitoring. Stop monitoring thread and set metrics to 0.
+
+        Parameters
+        ----------
+        task_instance: TaskInstance
+            Task instance object.
+        """
+
+        if not self.check_plugin_enabled:
+            return
+
+        if task_instance in self.monitor_threads:
+            monitor_info = self.monitor_threads.pop(task_instance, {})
+            if monitor_info:
+                monitor_thread, stop_monitoring = monitor_info
+                stop_monitoring.set()
+                if monitor_thread.is_alive():
+                    monitor_thread.join()
+
+            Stats.gauge(self.get_metric_name(task_instance, "custom_metrics_cpu_usage_percent"), 0)
+            Stats.gauge(self.get_metric_name(task_instance, "custom_metrics_memory_usage_bytes"), 0)
+
+            logging.info(
+                f"===== 📊🏁CUSTOM_METRICS_PLUGIN. Task {task_instance.task_id} in DAG {task_instance.dag_id} completed ====="
+            )
+
+    @hookimpl
+    def on_task_instance_running(self, previous_state, task_instance, session, **kwargs):
+        if not self.check_plugin_enabled:
+            return
+
+        import os
+        import threading
+        import time
+
+        import psutil
+
+        sleep_interval_seconds = int(os.getenv("CUSTOM_METRICS_PLUGIN_SLEEP_INTERVAL_SECONDS", 1))
+
+        logging.info(
+            f"===== 📊▶️CUSTOM_METRICS_PLUGIN. Task {task_instance.task_id} in DAG {task_instance.dag_id} is starting ====="
+        )
+
+        process = psutil.Process(task_instance.pid)
+        stop_monitoring = threading.Event()
+
+        def monitor():
+            while not stop_monitoring.is_set():
+                if not process.is_running() or task_instance.state in STOPPED_TASK_INSTANCES_STATUSES:
+                    break
+                memory_usage = process.memory_info().rss  # Used memory by process by bytes
+                cpu_usage = process.cpu_percent(interval=1)  # CPU usage by process in percentage
+
+                Stats.gauge(self.get_metric_name(task_instance, "custom_metrics_cpu_usage_percent"), cpu_usage)
+                Stats.gauge(self.get_metric_name(task_instance, "custom_metrics_memory_usage_bytes"), memory_usage)
+
+                logging.info(
+                    f"===== 📊CUSTOM_METRICS_PLUGIN. Memory usage: {convert_bytes_to_readable_value(memory_usage)}, CPU usage: {cpu_usage:.2f}% ====="
+                )
+                time.sleep(sleep_interval_seconds)
+
+        monitor_thread = threading.Thread(target=monitor, daemon=True)
+        monitor_thread.start()
+
+        self.monitor_threads[task_instance] = (monitor_thread, stop_monitoring)
+
+    @hookimpl
+    def on_task_instance_success(self, previous_state, task_instance, session, **kwargs):
+        self._finalize_task(task_instance)
+
+    @hookimpl
+    def on_task_instance_failed(self, previous_state, task_instance, **kwargs):
+        self._finalize_task(task_instance)
+
+    @hookimpl
+    def before_stopping(self, **kwargs):
+        # - Stop all monitoring threads for tasks that were skipped, deferred, removed, etc.
+        for task_instance in self.monitor_threads:
+            if task_instance.state in STOPPED_TASK_INSTANCES_STATUSES:
+                self._finalize_task(task_instance)
+
+
+class CustomMetricsPlugin(AirflowPlugin):
+    name = "CUSTOM_METRICS_PLUGIN"
+    listeners = [TaskMonitorListener()]
